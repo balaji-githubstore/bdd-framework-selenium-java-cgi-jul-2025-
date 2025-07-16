dfc292c4b9be2e9b9097eeda9a30c365fe34bce7
balaji-githubstore <balajidinakaran1@gmail.com>
2025-07-16 07:44:45 +0000

    BDD framework - Without automation

--- src/test/java/com/cgi/steps/EmployeeSteps.java
+++ src/test/java/com/cgi/steps/EmployeeSteps.java
@@ -1,5 +1,8 @@
 package com.cgi.steps;
 
+import java.util.List;
+import java.util.Map;
+
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,23 +13,36 @@ public class EmployeeSteps {
 	public void user_click_on_pim_menu() {
 	  System.out.println("pim menu");
 	}
+	
 	@When("User click on Add Employee")
 	public void user_click_on_add_employee() {
 	    
 	}
+	
 	@When("User fill the employee form")
 	public void user_fill_the_employee_form(DataTable dataTable) {
 		//fill the firstname,lastname,middlename 
 	    System.out.println(dataTable);
+	    
+	    List<Map<String,String>> lists=dataTable.asMaps();
+	    System.out.println(lists);
+	    
+	    System.out.println(lists.get(0));
+	    System.out.println(lists.get(0).get("firstname"));
+	    System.out.println(lists.get(0).get("middlename"));
+	    System.out.println(lists.get(0).get("lastname"));
 	}
+	
 	@When("User clicks on save employee")
 	public void user_clicks_on_save_employee() {
 	    
 	}
+	
 	@Then("User should get the profile name as {string}")
 	public void user_should_get_the_profile_name_as(String string) {
 	    
 	}
+	
 	@Then("User should get the personal details as per the form filled")
 	public void user_should_get_the_personal_details_as_per_the_form_filled() {
 	    
